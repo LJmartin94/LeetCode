ef4ee450d63fca64d13df11bb58da683d3b43cef
Lindsay <141155135+LindsayJamesMartin-TomTom@users.noreply.github.com>
2025-01-28 15:54:05 +0100

    Remove superfluous variable

--- 1TwoSum/Solution2.kt
+++ 1TwoSum/Solution2.kt
@@ -1,16 +1,14 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val numbersVisited = mutableMapOf<Int, Int>()
-        var i: Int? = null
         var j: Int? = null
 
         for (num in nums.withIndex()){
-            i = num.index
             j = numbersVisited.get(target - num.value)
             if (j != null) {
                 println("Map of visited numbers: $numbersVisited")
-                println("Required index $j found to sum with index $i")
-                return intArrayOf(i, j)
+                println("Required index $j found to sum with index ${num.index}")
+                return intArrayOf(num.index, j)
             }
             numbersVisited.put(num.value, num.index)
         }
